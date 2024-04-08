cb4307ab11e3358a5eb656232fab3b427aae95c7
Mathias Djärv <mathias.djarv@gmail.com>
2024-04-08 13:51:53 +0200

    add layer indicator tiles

--- keyboards/crkbd/keymaps/mdjarv2/config.h
+++ keyboards/crkbd/keymaps/mdjarv2/config.h
@@ -52,3 +52,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SPLIT_LED_STATE_ENABLE
 #define SPLIT_LAYER_STATE_ENABLE
 #define SPLIT_MODS_ENABLE
+
+#define TAPPING_TERM 100
